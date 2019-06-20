8c12a1e9d2685e47cda8213938d1494e1fe305bd
LoicBrassart <loic.brassart@gmail.com>
2019-06-20 18:59:39 +0200

    Modded jwtSecret key + added saltRounds key.

--- backend/conf.js
+++ backend/conf.js
@@ -7,7 +7,8 @@ CONFIG.dbDialect = process.env.DB_DIALECT || "mysql";
 CONFIG.dbHost = process.env.DB_HOST || "localhost";
 CONFIG.dbPort = process.env.DB_PORT || "3306";
 CONFIG.dbName = process.env.DB_NAME || "name";
-CONFIG.jwtEncryption = process.env.JWT_ENCRYPTION || "jwt_please_change";
+CONFIG.jwtSecret = process.env.JWT_SECRET || "jwt_please_change";
+CONFIG.saltRounds = process.env.SALT_ROUNDS || "20";
 CONFIG.jwtExpiration = process.env.JWT_EXPIRATION || "10000";
 
 const dbUrl = `mongodb://${CONFIG.dbHost}:${CONFIG.dbPortNumber}/${
