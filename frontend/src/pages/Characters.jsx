5f6f0cf498b8ca7ba1832677844c7f9977007b64
LoicBrassart <loic.brassart@gmail.com>
2019-06-29 21:46:48 +0200

    Added lazyloading to the Characters page.

--- frontend/src/pages/Characters.jsx
+++ frontend/src/pages/Characters.jsx
@@ -1,22 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import "./styles/Characters.scss";
 import Character from "../components/Character";
 import Title from "../components/Title";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 class Characters extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      charactersData: []
+      charactersData: [],
+      api: {
+        page: 1,
+        hasMore: true
+      }
     };
   }
   componentDidMount() {
+    this.fetchMoreCharacters();
+  }
+
+  fetchMoreCharacters() {
     axios
-      .get("http://localhost:5050/character")
+      .get(`http://localhost:5050/character?page=${this.state.api.page}`)
       .then(({ data }) => {
         this.setState({
-          charactersData: data
+          charactersData: [...this.state.charactersData, ...data],
+          api: {
+            page: parseInt(this.state.api.page) + 1,
+            hasMore: data.length > 0
+          }
         });
       })
       .catch(err => {
@@ -25,14 +38,39 @@ class Characters extends React.Component {
   }
 
   render() {
-    return (
-      <div className="Characters">
-        <Title label="Personnages" />
+    /*
         <main>
           {this.state.charactersData.map((char, i) => (
             <Character key={i} charData={char} />
           ))}
         </main>
+    
+    */
+    return (
+      <div className="Characters">
+        <Title label="Personnages" />
+        <InfiniteScroll
+          dataLength={this.state.charactersData.length} //This is important field to render the next data
+          next={() => {
+            this.fetchMoreCharacters();
+          }}
+          hasMore={this.state.api.hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>
+                Désolé... C'est un peu gênant, mais on n'a plus aucun perso à te
+                proposer !
+              </b>
+            </p>
+          }
+        >
+          <main>
+            {this.state.charactersData.map((char, i) => (
+              <Character key={i} charData={char} />
+            ))}
+          </main>
+        </InfiniteScroll>
       </div>
     );
   }
