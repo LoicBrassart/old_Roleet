fa341a5da6b39a2c9bea1c035d9e218eb37199b4
LoicBrassart <loic.brassart@gmail.com>
2019-07-04 11:55:32 +0200

    Added support for lock situations (logged out + user not in the 'readers')

--- frontend/src/components/Scenario.jsx
+++ frontend/src/components/Scenario.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./styles/Scenario.scss";
 
-const Scenario = ({ scenData: { title, summary, created_at } }) => {
+const Scenario = ({ scenData: { title, summary, created_at }, locked }) => {
   return (
     <div className="Scenario">
       <header>
-        <h2>{title}</h2>
-        <p>{new Date(created_at).toDateString()}</p>
+        <div>
+          <h2>{title}</h2>
+          <p>{new Date(created_at).toDateString()}</p>
+        </div>
+        <button disabled={locked}>Lire</button>
       </header>
       <p>{summary}</p>
     </div>
