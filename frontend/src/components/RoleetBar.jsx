9f170560e5fdd14e422731ff175da102282fae42
LoicBrassart <loic.brassart@gmail.com>
2019-07-23 20:09:45 +0200

    Deleted all the logic about the search engine

--- frontend/src/components/RoleetBar.jsx
+++ frontend/src/components/RoleetBar.jsx
@@ -1,60 +1,9 @@
 import React from "react";
 import "./styles/RoleetBar.scss";
 import { NavLink, Link } from "react-router-dom";
-import cogoToast from "cogo-toast";
-import { api } from "../conf";
 import Search from "./Search";
 
 class RoleetBar extends React.Component {
-  onChangeSearchString(e) {
-    this.setState({ search: { ...this.state.search, str: e.target.value } });
-    api
-      .get(`/character?search=${e.target.value}`)
-      .then(({ data }) => {
-        this.setState({
-          search: { ...this.state.search, characters: data }
-        });
-      })
-      .catch(err => {
-        cogoToast.error(
-          `Une erreur est survenue lors de la récupération: ${err}`,
-          {
-            position: "bottom-right"
-          }
-        );
-      });
-    api
-      .get(`/user?search=${e.target.value}`)
-      .then(({ data }) => {
-        this.setState({
-          search: { ...this.state.search, users: data }
-        });
-      })
-      .catch(err => {
-        cogoToast.error(
-          `Une erreur est survenue lors de la récupération: ${err}`,
-          {
-            position: "bottom-right"
-          }
-        );
-      });
-    api
-      .get(`/scenario?search=${e.target.value}`)
-      .then(({ data }) => {
-        this.setState({
-          search: { ...this.state.search, scenarii: data }
-        });
-      })
-      .catch(err => {
-        cogoToast.error(
-          `Une erreur est survenue lors de la récupération: ${err}`,
-          {
-            position: "bottom-right"
-          }
-        );
-      });
-  }
-
   render() {
     return (
       <nav className="RoleetBar">
@@ -68,7 +17,7 @@ class RoleetBar extends React.Component {
           </NavLink>
         </div>
         <ul className="flexer">
-          <li>
+          <li className="center">
             <Search />
           </li>
           <li className="center">
