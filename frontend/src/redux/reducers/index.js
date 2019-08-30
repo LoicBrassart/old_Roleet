750f452acc4649eb91d4ba2c93dae8395f54cfee
LoicBrassart <loic.brassart@gmail.com>
2019-08-30 22:20:24 +0200

    Added form reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import { reducer as form } from "redux-form";
 
 import modal from "./modalReducer";
 import user from "./userReducer";
 
 export default combineReducers({
   modal,
-  user
+  user,
+  form
 });
